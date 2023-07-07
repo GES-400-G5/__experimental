11d42e023b309d1568a139f3f11e611885c10f9c
peteruche21 <anyaogupeter601@gmail.com>
2023-07-07 23:01:24 +0100

    trial-and-error-2 remove-create-browser-router

--- src/providers/router.tsx
+++ src/providers/router.tsx
@@ -1,19 +1,34 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  // createBrowserRouter,
+  // createRoutesFromElements,
+  // RouterProvider,
+} from "react-router-dom";
 import Landing from "../pages";
 import About from "../pages/about";
 import Team from "../pages/team";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/">
-      <Route index element={<Landing />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/team" element={<Team />} />
-    </Route>
-  ),
-  { basename: "/__experimental/" }
-);
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/">
+//       <Route index element={<Landing />} />
+//       <Route path="/about" element={<About />} />
+//       <Route path="/team" element={<Team />} />
+//     </Route>
+//   ),
+//   { basename: "/__experimental/" }
+// );
 
 export const RouteProvider = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter basename={"/__experimental/"}>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/team" element={<Team />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
