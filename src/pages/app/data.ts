18198886d9858cd2515ca285d05066479e05adbb
peteruche21 <anyaogupeter601@gmail.com>
2023-07-08 07:39:19 +0100

    help: data.ts

---
+++ src/pages/app/data.ts
@@ -0,0 +1,37 @@
+export const resources = ["LECTURE NOTES", "PAST QUESTIONS", "MATERIALS"];
+export const semesters = ["1st Semester", "2nd Semester"];
+const levels = [100, 200, 300, 400];
+
+export const semester = {
+  semester1: {
+    100: {
+      courses: [
+        "GES 100.1 Computer Skill in English",
+        "GES 102.1 Introduction to Logic & Philosophy",
+        "MTH 110.1 Algebra and Trigonometry",
+        "MTH 120.1 Calculus",
+        "PHY 101.1 Mechanics and Properties of Matter",
+        "CSC 180.1 Introduction to Computer Science and Basic Programming",
+        "CHM 130.1 General Chemistry",
+        "FSB 101.1 General Biology 1",
+      ],
+      materials: [
+        {
+          name: "GES 100.1",
+          lecture_notes: "",
+          materials: "",
+          past_questions: "",
+        },
+      ],
+    },
+    200: {},
+    300: {},
+    400: {},
+  },
+  semester2: {
+    100: {},
+    200: {},
+    300: {},
+    400: {},
+  },
+};
