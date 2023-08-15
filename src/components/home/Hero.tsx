a069e6880ebe89a62ccae8448955919cebfe61f3
peteruche21 <anyaogupeter601@gmail.com>
2023-08-15 12:50:05 +0100

    hero image with overlay

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -3,7 +3,7 @@ import Button from "../ui/Button";
 const Hero = (): JSX.Element => {
   return (
     <div className="w-full pt-8 px-8">
-      <div className="max-w-[1093px] mx-auto flex flex-col md:flex-row justify-between items-center gap-16">
+      <div className="max-w-[1093px] mx-auto flex flex-col md:flex-row justify-between items-center gap-10">
         <div className="w-[356px] h-[351px] mdw-[586px] md:h-[407px] py-2.5 flex-col justify-start items-start gap-6 inline-flex">
           <div className="pr-2.5 justify-start items-center gap-2.5 inline-flex">
             <div className="md:w-[527px] w-[346px] md:text-[64px] self-stretch text-blue-950 text-[42px] satoshi-bold">
@@ -23,11 +23,15 @@ const Hero = (): JSX.Element => {
           <div className="relative min-h-[451px] md:min-h-[556px] w-96 md:w-96 md:pt-0">
             <div className="absolute md:w-[78.28px] md:h-[78.28px] w-[61.90px] h-[61.90px] left-[35px] top-0  bg-sky-500 bg-opacity-10 rounded-full" />
             <div className="absolute w-[240px] h-[240px] md:w-[270px] md:h-[270px] top-[45px] left-[125px] md:left-[115px] bg-sky-500 bg-opacity-10 rounded-full" />
-            <div className=" absolute top-[60.44px] right-[45.25px]">
-              <img
-                className="w-[300px] md:w-[556.69px] rounded-full"
-                src="/__experimental/assets/study-group-african-people 1.png"
-              />
+            <div className="relative">
+              <div className="absolute top-[60.44px] right-[45.25px]">
+                <img
+                  className="w-[300px] h-[300px] md:w-[556.69px] rounded-full"
+                  src="/__experimental/assets/stimage.jpg"
+                  alt="Image"
+                />
+              </div>
+              <div className="absolute top-[60.44px] right-[45.25px] md:w-[340px] w-[300px] h-[300px]  rounded-full bg-black opacity-50"></div>
             </div>
           </div>
         </div>
